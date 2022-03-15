dce3d83e933ae8ec21521bc8a0c8c57dae8dcd81
Alex <alexandru.gatu@gmail.com>
2022-03-15 09:20:48 +0200

    Creation of framework structure

---
+++ JuiceShopAutomation/Tests/Authentication/AuthenticationTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using JuiceShopAutomation.Tests;
+using JuiceShopAutomation.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuiceShopAutomation.Tests.Authentication
+{
+    public class AuthenticationTests : BaseTest
+    {
+
+        string url = FrameworkConstants.GetUrl();
+
+        [Test]
+        public void AuthenticationPositive()
+        {
+            testName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(testName);
+            _driver.Navigate().GoToUrl(url);
+        }
+
+
+    }
+}
